eacb238f28f7cef3643e16814c36243ac250c521
Joakker <Joakker@users.noreply.github.com>
2025-02-16 17:42:19 +0000

    deploy: f7a22ab36cae8f7ca01f8bff4d4e73d5d4d6b017

---
+++ toc.js
@@ -0,0 +1,70 @@
+// Populate the sidebar
+//
+// This is a script, and not included directly in the page, to control the total size of the book.
+// The TOC contains an entry for each page, so if each page includes a copy of the TOC,
+// the total size of the page becomes O(n**2).
+class MDBookSidebarScrollbox extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="Summary/installation.html">Installation</a></li><li class="chapter-item expanded affix "><li class="part-title">Quick Start</li><li class="chapter-item expanded "><a href="Summary/managing-scripts.html"><strong aria-hidden="true">1.</strong> Managing Scripts</a></li><li class="chapter-item expanded "><a href="Summary/running-scripts.html"><strong aria-hidden="true">2.</strong> Running Scripts</a></li><li class="chapter-item expanded "><a href="Summary/controlling-script-bindings.html"><strong aria-hidden="true">3.</strong> Controlling Script Bindings</a></li><li class="chapter-item expanded "><a href="Summary/customizing-script-contexts.html"><strong aria-hidden="true">4.</strong> Modifying Script Contexts</a></li><li class="chapter-item expanded "><a href="Summary/sharing-contexts-between-scripts.html"><strong aria-hidden="true">5.</strong> Shared Contexts</a></li><li class="chapter-item expanded "><a href="Summary/script-id-mapping.html"><strong aria-hidden="true">6.</strong> Script ID Mapping</a></li><li class="chapter-item expanded affix "><li class="part-title">Scripting Reference</li><li class="chapter-item expanded "><a href="ScriptingReference/introduction.html"><strong aria-hidden="true">7.</strong> Introduction</a></li><li class="chapter-item expanded "><a href="ScriptingReference/core-api.html"><strong aria-hidden="true">8.</strong> Core Bindings</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="ScriptingReference/world.html"><strong aria-hidden="true">8.1.</strong> World</a></li><li class="chapter-item expanded "><a href="ScriptingReference/reflect-reference.html"><strong aria-hidden="true">8.2.</strong> ReflectReference</a></li><li class="chapter-item expanded "><a href="ScriptingReference/script-type-registration.html"><strong aria-hidden="true">8.3.</strong> ScriptTypeRegistration</a></li><li class="chapter-item expanded "><a href="ScriptingReference/script-component-registration.html"><strong aria-hidden="true">8.4.</strong> ScriptComponentRegistration</a></li><li class="chapter-item expanded "><a href="ScriptingReference/script-resource-registration.html"><strong aria-hidden="true">8.5.</strong> ScriptResourceRegistration</a></li><li class="chapter-item expanded "><a href="ScriptingReference/script-query-builder.html"><strong aria-hidden="true">8.6.</strong> ScriptQueryBuilder</a></li><li class="chapter-item expanded "><a href="ScriptingReference/script-query-result.html"><strong aria-hidden="true">8.7.</strong> ScriptQueryResult</a></li></ol></li><li class="chapter-item expanded "><a href="ScriptingReference/core-callbacks.html"><strong aria-hidden="true">9.</strong> Core Callbacks</a></li><li class="chapter-item expanded affix "><li class="part-title">Developing BMS</li><li class="chapter-item expanded "><a href="Development/introduction.html"><strong aria-hidden="true">10.</strong> Introduction</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="Development/setup.html"><strong aria-hidden="true">10.1.</strong> Setup</a></li></ol></li><li class="chapter-item expanded "><a href="Development/AddingLanguages/introduction.html"><strong aria-hidden="true">11.</strong> New Languages</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="Development/AddingLanguages/evaluating-feasibility.html"><strong aria-hidden="true">11.1.</strong> Evaluating Feasibility</a></li><li class="chapter-item expanded "><a href="Development/AddingLanguages/necessary-features.html"><strong aria-hidden="true">11.2.</strong> Necessary Features</a></li></ol></li></ol>';
+        // Set the current, active page, and reveal it if it's hidden
+        let current_page = document.location.href.toString().split("#")[0];
+        if (current_page.endsWith("/")) {
+            current_page += "index.html";
+        }
+        var links = Array.prototype.slice.call(this.querySelectorAll("a"));
+        var l = links.length;
+        for (var i = 0; i < l; ++i) {
+            var link = links[i];
+            var href = link.getAttribute("href");
+            if (href && !href.startsWith("#") && !/^(?:[a-z+]+:)?\/\//.test(href)) {
+                link.href = path_to_root + href;
+            }
+            // The "index" page is supposed to alias the first chapter in the book.
+            if (link.href === current_page || (i === 0 && path_to_root === "" && current_page.endsWith("/index.html"))) {
+                link.classList.add("active");
+                var parent = link.parentElement;
+                if (parent && parent.classList.contains("chapter-item")) {
+                    parent.classList.add("expanded");
+                }
+                while (parent) {
+                    if (parent.tagName === "LI" && parent.previousElementSibling) {
+                        if (parent.previousElementSibling.classList.contains("chapter-item")) {
+                            parent.previousElementSibling.classList.add("expanded");
+                        }
+                    }
+                    parent = parent.parentElement;
+                }
+            }
+        }
+        // Track and set sidebar scroll position
+        this.addEventListener('click', function(e) {
+            if (e.target.tagName === 'A') {
+                sessionStorage.setItem('sidebar-scroll', this.scrollTop);
+            }
+        }, { passive: true });
+        var sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
+        sessionStorage.removeItem('sidebar-scroll');
+        if (sidebarScrollTop) {
+            // preserve sidebar scroll position when navigating via links within sidebar
+            this.scrollTop = sidebarScrollTop;
+        } else {
+            // scroll sidebar to current active section when navigating via "next/previous chapter" buttons
+            var activeSection = document.querySelector('#sidebar .active');
+            if (activeSection) {
+                activeSection.scrollIntoView({ block: 'center' });
+            }
+        }
+        // Toggle buttons
+        var sidebarAnchorToggles = document.querySelectorAll('#sidebar a.toggle');
+        function toggleSection(ev) {
+            ev.currentTarget.parentElement.classList.toggle('expanded');
+        }
+        Array.from(sidebarAnchorToggles).forEach(function (el) {
+            el.addEventListener('click', toggleSection);
+        });
+    }
+}
+window.customElements.define("mdbook-sidebar-scrollbox", MDBookSidebarScrollbox);
