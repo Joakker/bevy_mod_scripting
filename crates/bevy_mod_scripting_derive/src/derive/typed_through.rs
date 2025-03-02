a401bee4dc45b8585e4c1a9779c9fd8badecf2fe
Joakker <joaquinandresleon108@gmail.com>
2025-03-02 12:01:52 -0300

    fix(derive): Call `type_info()` method correctly in `#[derive(TypedThrough)]` expansion

--- crates/bevy_mod_scripting_derive/src/derive/typed_through.rs
+++ crates/bevy_mod_scripting_derive/src/derive/typed_through.rs
@@ -12,11 +12,12 @@ pub fn typed_through(input: TokenStream) -> TokenStream {
 
     let (impl_generics, type_generics, where_clause) = generics.split_for_impl();
 
-    let turbofish = type_generics.as_turbofish();
     quote! {
         impl #impl_generics ::bevy_mod_scripting::core::docgen::typed_through::TypedThrough for #ident #type_generics #where_clause {
             fn through_type_info() -> ::bevy_mod_scripting::core::docgen::typed_through::ThroughTypeInfo {
-                ::bevy_mod_scripting::core::docgen::typed_through::ThroughTypeInfo::TypeInfo(#ident #turbofish ::type_info())
+                ::bevy_mod_scripting::core::docgen::typed_through::ThroughTypeInfo::TypeInfo(
+                    <Self as bevy::reflect::Typed>::type_info()
+                )
             }
         }
     }
